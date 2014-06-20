392af883fca6cf574c0e08c1e495ffb9c9ada8ff
Will Riley <will@hflw.org>
2014-06-20 14:55:37 -0400

    fixed problem where btport wasn't being accepted as an argument for Ev3_base

--- module/Ev3.js
+++ module/Ev3.js
@@ -149,7 +149,7 @@ var Ev3_base = function(btport){
 	this.OUTPUT_BODY_SEQ = "407E018200008"; 
 	this.TERMINATE_SEQ = new Buffer("070055008000000201","hex");
 
-	this.sp = new SerialPort("/dev/rfcomm1", {
+	this.sp = new SerialPort(btport, {
 		  parser: SP.parsers.raw
 	}, false); 
 
